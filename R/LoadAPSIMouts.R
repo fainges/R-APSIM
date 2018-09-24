f120f2512ffb5df595bf98656fb415d9f2964113
Adam H. Sparks <adamhsparks@gmail.com>
2018-09-24 11:12:37 +1000

    Removes `compiler::cmpfun()` since now in DESCRIPTION

--- R/LoadAPSIMouts.R
+++ R/LoadAPSIMouts.R
@@ -1,15 +1,15 @@
 #' Read APSIM .out files.
-#' 
+#'
 #' Reads APSIM .out files.
-#' 
-#' By default, this function will read in all output files in a directory and 
+#'
+#' By default, this function will read in all output files in a directory and
 #' combine them into a single data table. Setting \code{loadAll=FALSE} will read
 #' a single file. If the path is omitted it will try to find the file in the current
-#' working directory. Constants in outputs will be added as data columns and output 
-#' files with differing numbers of columns can also be imported although by 
-#' default this will result in an error. Care should be taken when using this 
-#' option as a different number of columns in output files usually means the 
-#' data came from a different set of reports or simulations which may not be 
+#' working directory. Constants in outputs will be added as data columns and output
+#' files with differing numbers of columns can also be imported although by
+#' default this will result in an error. Care should be taken when using this
+#' option as a different number of columns in output files usually means the
+#' data came from a different set of reports or simulations which may not be
 #' relevant to your analysis.
 #' @param dir (optional) The directory to search for .out files. This is not recursive.
 #'   If omitted, the current working directory will be used.
@@ -17,27 +17,27 @@
 #'   a single file specified by \code{dir}. Default is TRUE.
 #' @param filter A regular expression that matches the files to be loaded.
 #'   Default is \code{\\\\.out} which filters for standard apsim output files.
-#' @param returnFrame Return the data as a data frame or data table. Default is 
+#' @param returnFrame Return the data as a data frame or data table. Default is
 #'   TRUE, FALSE returns a data table.
-#' @param n Read the first n files. Good for testing/debugging. Default is 0 
+#' @param n Read the first n files. Good for testing/debugging. Default is 0
 #'   (read all files found).
-#' @param fill Where the number or names of columns is not consistent across 
-#'   files, fill missing columns with NA. Default is FALSE which will throw an 
+#' @param fill Where the number or names of columns is not consistent across
+#'   files, fill missing columns with NA. Default is FALSE which will throw an
 #'   error if the columns don't match across all files.
-#' @param addConstants Add any constants (such as ApsimVersion, Title or factor 
+#' @param addConstants Add any constants (such as ApsimVersion, Title or factor
 #'   levels) found as extra columns. Default is TRUE.
 #' @param skipEmpty Silently skip empty output files. If false, empty files
 #'   will cause an error. Default is FALSE.
 #' @export
 #' @examples
 #' \dontrun{loadApsim("c:/outputs") # load everything in the outputs directory}
-#' \dontrun{loadApsim("c:/outputs/simulation.out", loadAll=FALSE) 
+#' \dontrun{loadApsim("c:/outputs/simulation.out", loadAll=FALSE)
 #' # load a single file (note extension is required).}
-#' \dontrun{loadApsim("c:/outputs", returnFrame=FALSE, fill=TRUE) 
+#' \dontrun{loadApsim("c:/outputs", returnFrame=FALSE, fill=TRUE)
 #'   # load everything in the outputs directory, fill any missing columns and return a data table.}
-loadApsim <- compiler::cmpfun(function(dir = NULL, loadAll=TRUE, filter = "\\.out", returnFrame = TRUE, n = 0, fill=FALSE, addConstants=TRUE, skipEmpty=FALSE)
+loadApsim <- function(dir = NULL, loadAll=TRUE, filter = "\\.out", returnFrame = TRUE, n = 0, fill=FALSE, addConstants=TRUE, skipEmpty=FALSE)
 {    # this function is precompiled for a 10-12% performance increase
-    if (loadAll){ 
+    if (loadAll){
         wd <- getwd()
         if(!is.null(dir)){
             setwd(dir)
@@ -51,10 +51,10 @@ loadApsim <- compiler::cmpfun(function(dir = NULL, loadAll=TRUE, filter = "\\.ou
     }
     if (length(files == 0))
         return
-    
+
     allData <- list(NULL)
     fileCount <- 0
-    
+
     for(f in files) {
         print(f)
         if(skipEmpty && file.info(f)$size == 0)
@@ -65,7 +65,7 @@ loadApsim <- compiler::cmpfun(function(dir = NULL, loadAll=TRUE, filter = "\\.ou
         constants <- NULL
         namesFound <- FALSE
         unitsFound <- FALSE
-        
+
         while (length(oneLine <- readLines(con, n=1, warn=FALSE)) > 0) {
             if(grepl("factors = ", oneLine)){ # line contains a single line factor string
                 if(addConstants){
@@ -89,7 +89,7 @@ loadApsim <- compiler::cmpfun(function(dir = NULL, loadAll=TRUE, filter = "\\.ou
                         units <- subset(units, units != "")
                         # this shouldn't (but can) happen. e.g. (DECIMAL DEGREES) when reporting lat/lon
                         if(length(units) != length(colNames)) stop(paste("Error reading", f, "number of columns does not match number of headings."))
-                        unitsFound <- TRUE          
+                        unitsFound <- TRUE
                     } else {    # everything else is data
                         break
                     }
@@ -122,20 +122,20 @@ loadApsim <- compiler::cmpfun(function(dir = NULL, loadAll=TRUE, filter = "\\.ou
     })
     if (loadAll) setwd(wd) #restore wd
     return(allData)
-})
+}
 
 #' Extract a token from a vector of strings.
-#' 
+#'
 #' Extract a token from a character vector in a data frame containing a number
 #' of delimited tokens. Specialised version of str_split that works on vectors.
-#' 
+#'
 #' By default loadApsim will automatically create columns from constants inside
-#' the output files. However sometimes you might want to extract values from 
+#' the output files. However sometimes you might want to extract values from
 #' different data such as the file name. This is easy to do if the data is
 #' a fixed width, but it can be difficult when it's of variable length and
-#' delimited. str_split() is the most common way to deal with this sort of 
+#' delimited. str_split() is the most common way to deal with this sort of
 #' data, but it doesn't work on a vector.
-#' 
+#'
 #' getToken takes a data frame, a column of type 'character', a seperator and a token position to return
 #' each token in the given position. It then appends the token as a new column.
 #' @param x A data frame
@@ -146,14 +146,14 @@ loadApsim <- compiler::cmpfun(function(dir = NULL, loadAll=TRUE, filter = "\\.ou
 getToken <- function(x, cname, pos, sep, colName) {
     if (is.null(cname) | length(cname) != 1)
         stop("cname must be a character vector of length 1.")
-    
+
     delims <- str_count(x[, cname], sep)
-    
+
     if (min(delims) != max(delims))
         stop ("Number of delimiters is not the same in all vector elements.")
-    
+
     m<- str_split_fixed(x[, cname], "_",n= delims + 1)
-    
+
     x <- cbind(x, m[, pos])
     setnames(x, "m[, pos]", colName)
     return(x)
